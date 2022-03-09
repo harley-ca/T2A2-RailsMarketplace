704d842600bb48d7d443b65c4783b7761fc2d82f
harley-ca <gcab022110@coderacademy.edu.au>
2022-03-09 16:12:04 +1000

    commiting changes before adding review model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,6 @@
 class Listing < ApplicationRecord
   belongs_to :user
+
+  enum listing_type: [:coach, :offer, :request, :trade]
+  enum status: [:draft, :sent, :archived, :accepted, :declined, :open, :closed]
 end
