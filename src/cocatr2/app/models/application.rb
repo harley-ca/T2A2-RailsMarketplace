c6f12977f9b663617ddc96c6fa65370fee4d8197
harley-ca <gcab022110@coderacademy.edu.au>
2022-03-12 23:02:36 +1000

    minor styling work on buttons

--- src/cocatr2/app/models/application.rb
+++ src/cocatr2/app/models/application.rb
@@ -3,6 +3,6 @@ class Application < ApplicationRecord
   belongs_to :listing
 
   enum application_type: { "Application" => 0, "Report" => 1 }
-  enum status: { "Draft" => 0, "Pending" => 1, "Approved" => 2, "Declined" => 3, "Archived" => 4, "Open" => 5, "Closed" => 6 }
+  enum status: { "Draft" => 0, "Pending" => 1, "Approved" => 2, "Declined" => 3, "Open" => 4, "Archived" => 5, "Closed" => 6 }
 
 end
