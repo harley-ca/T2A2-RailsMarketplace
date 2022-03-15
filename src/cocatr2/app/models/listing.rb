6ed98b92a442fcd2a4347d56f15789f72d83147b
harley-ca <gcab022110@coderacademy.edu.au>
2022-03-15 18:32:03 +1000

    added pg_search to simplify title searching, new plan is to have search form on listings#index

--- src/cocatr2/app/models/listing.rb
+++ src/cocatr2/app/models/listing.rb
@@ -1,15 +1,18 @@
 class Listing < ApplicationRecord
-include Filterable
+#include Filterable
+include PgSearch::Model
   belongs_to :user
   has_many :reviews
   has_many :applications
 
-    # Enums define what the integer values mean, and provide an alternate way to use them.
+  # Enums define what the integer values mean, and provide an alternate way to use them
   enum listing_type: { "Coach" => 0, "Offer" => 1, "Request" => 2, "Trade" => 3 }
   enum status: { "Open" => 0, "Closed" => 1, "Archived" => 2, "Removed" => 3}
 
-  # Scopes for searching
-  scope :filter_by_title, -> (title) {where("title like ?", "#{title}")}
+  # pg_search scope to be a bit more flexible for the search bar
+  pg_search_scope :filter_by_title, against: :title
+
+  # Regular scopes for assorted views
   scope :filter_by_status, -> (status) {where status: status}
   scope :filter_by_listing_type, -> (listing_type) {where listing_type: listing_type}
   scope :filter_by_username, -> (username) { joins(:user).where('username like ?', "#{username}")}
